a87e37b15f4d54eab96f5fd276b9549ba91194a9
Yunus <yunus.ahmedov.11@mail.ru>
2025-04-16 00:35:36 +0400

    added updates

--- src/pages/ServicesPage/ServicesPage.jsx
+++ src/pages/ServicesPage/ServicesPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import PagesContent from '../../components/PagesContent'
 
 function ServicesPage() {
   return (
 
    <div className='full-container'>
     <Header />
-    
 
     <Footer/>
     </div>
